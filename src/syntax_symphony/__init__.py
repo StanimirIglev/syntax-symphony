d0976ac60391ed0cbfb65a31f23d9702214f81ef
Stanimir Iglev <iglevv@gmail.com>
2026-09-02 11:04:48 +0200

    feat: expose version over public interface

--- src/syntax_symphony/__init__.py
+++ src/syntax_symphony/__init__.py
@@ -1,12 +1,17 @@
 """Efficient grammar-based fuzzer with k-path coverage."""
 
+from importlib.metadata import version
+
 from .derivation_tree import DT
 from .fuzzer import SyntaxSymphony
 from .grammar import Grammar, load_grammar_from_file
+
+__version__ = version("syntax_symphony")
 
 __all__ = [
     "DT",
     "Grammar",
     "SyntaxSymphony",
+    "__version__",
     "load_grammar_from_file",
 ]
